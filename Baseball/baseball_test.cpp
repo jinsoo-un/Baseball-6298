f075a48516fff877112491d91ba6fbb57a9be01e
see.jin <bobstar0@naver.com>
2025-07-02 13:10:07 +0900

    add exception

--- Baseball/baseball_test.cpp
+++ Baseball/baseball_test.cpp
@@ -1,6 +1,12 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
+#include "baseball_test.h"
 
 TEST(BaseballGame, TryGameTest) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(BaseballGame , ThrowExceptionWhenInputLengthIsUnmatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
