cce74b5331d70523fcdeebfc741e49bcf60a8598
see.jin <bobstar0@naver.com>
2025-07-02 13:15:28 +0900

    add invalid argument

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -7,5 +7,11 @@ public:
 		if (string.length() !=  3) {
 			throw length_error("Input length must be 3 characters.");
 		}
+
+		for (char ch : string) {
+			if (ch < '0' || ch > '9') {
+				throw invalid_argument("Input must contain only digits.");
+			}
+		}
 	}
 };
