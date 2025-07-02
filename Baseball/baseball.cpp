bff229d552d7e52fd3e16811ed4473628fddc8be
see.jin <bobstar0@naver.com>
2025-07-02 13:28:29 +0900

    refector exception

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -4,20 +4,29 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& guessNumber) {
-		if (guessNumber.length() !=  3) {
+		assertIllegalArgument(guessNumber);
+	}
+
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
+		if (guessNumber.length() != 3) {
 			throw length_error("Input length must be 3 characters.");
 		}
 
 		for (char ch : guessNumber) {
-			if (ch < '0' || ch > '9') {
-				throw invalid_argument("Input must contain only digits.");
-			}
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("Input must contain only digits.");
 		}
 
-		if (guessNumber[0] == guessNumber[1]
-			|| guessNumber[0] == guessNumber[2]
-			|| guessNumber[1] == guessNumber[2]) {
+		if (isDuplicatedNumber(guessNumber)) {
 			throw invalid_argument("Input must not contain duplicate digits.");
 		}
+	}
+
+	bool isDuplicatedNumber(const std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2];
 	}
 };
